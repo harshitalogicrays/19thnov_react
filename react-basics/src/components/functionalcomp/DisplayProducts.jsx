2cf40a3931554137cf79d62cd748dd6b68283865
harshita <harshita@logicrays.com>
2025-02-05 09:35:00 +0530

    05feb

---
+++ react-basics/src/components/functionalcomp/DisplayProducts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DisplayProducts = ({product}) => {
+  return (
+    <div>
+      {/* {JSON.stringify(product)} */}
+
+ 
+      
+    </div>
+  )
+}
+
+export default DisplayProducts
--- react-basics/src/components/functionalcomp/ListRendering.jsx
+++ react-basics/src/components/functionalcomp/ListRendering.jsx
@@ -1,8 +1,23 @@
 import React, { Fragment } from 'react'
 import DisplayList from './DisplayList'
+import Image1 from '/src/assets/images/a.jpg'
+import Image2 from '/src/assets/images/b.jpg'
+import Image3 from '/src/assets/images/c.jpeg'
+import Image4 from '/src/assets/images/d.jpg'
+import DisplayProducts from './DisplayProducts'
 
 const ListRendering = () => {
     const names = ["aaa","bbb","ccc","ddd","eeee","fff","ggg","hhh","aaa"]
+    const products = [
+                      {id:1,name:'product1',price:890,image:Image1 , category:'category1',stock:10},
+                      {id:2,name:'product2',price:1800,image:Image2 , category:'category2',stock:100},
+                      {id:3,name:'product3',price:1000,image:Image3 , category:'category3',stock:0},
+                      {id:4,name:'product4',price:800,image:Image4 , category:'category1',stock:20},
+                      {id:5,name:'product5',price:200,image:Image2 , category:'category2',stock:200},
+                      {id:6,name:'product6',price:900,image:Image1 , category:'category3',stock:90},
+                      {id:7,name:'product7',price:80,image:Image4 , category:'category4',stock:1},                    
+                    ] //array of objects
+    // console.log(products[0].name)
   return (
    <>
           {/* {names.join(",")} */}
@@ -11,8 +26,37 @@ const ListRendering = () => {
 
           {/* {names.map((value,index)=><Fragment key={index}>{value}</Fragment>)} */}
 
-            {names.map((val,i)=><DisplayList key={i} item={val} />)}
+            {/* {names.map((val,i)=><DisplayList key={i} item={val} />)} */}
 
+          {/* {JSON.stringify(products[0])} 
+          */}
+<div className="container col-8">
+<div class="table-responsive" >
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Image</th><th>Price</th>
+                    <th>Category</th><th>Stock</th>
+                </tr>
+            </thead>
+            <tbody>
+              {products.length==0 && <tr><td colspan={6} className='text-center'>No Product Found</td></tr>}
+              {products.map((product)=>
+                  <tr key={product.id}>
+                    <td>{product.id}</td>
+                    <td>{product.name}</td>
+                    <td><img src={product.image} height="50px" width="50px"/></td>
+                    <td>${product.price}</td>
+                    <td>{product.category}</td>
+                    <td>{product.stock}</td>
+                </tr>
+              )}
+          </tbody>
+        </table>
+      </div>
+      </div>
    </>
   )
 }
